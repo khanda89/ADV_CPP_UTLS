ab7ee7cc2f7aee5ba32a6acdb923cc0473e8894d
khanda89 <manoj.khanda89@gmail.com>
2021-06-12 05:59:26 -0700

    Update UtilFunctions.cpp

--- UtilFunctions.cpp
+++ UtilFunctions.cpp
@@ -3,6 +3,6 @@
 
 int UtilFunctions::GetMainMenuIndex()
 {
-    std::cout<<"I man being called in as sample"<< std::endl;
+    std::cout<<"I man being called in as sample1"<< std::endl;
     return 0;
 }
